47ccc8595bf4606ceaedb556931a07249ff5cd0e
BellCoutinho <bell.sccoutinho@gmail.com>
2021-12-20 18:00:48 -0300

    📦 - chore(Project Structure): Add initial project structure
    + Project structure
    + Spring Boot dependency
    + Spring Data dependency
    + Spring Web dependency
    + Lombok dependency
    + H2-database dependency
    + .gitIgnore

---
+++ src/main/java/com/ilearn/vle/VleApplication.java
@@ -0,0 +1,13 @@
+package com.ilearn.vle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilearn/vle/VleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilearn.vle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
